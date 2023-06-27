427b867fbed22ff3e2fe06bf2d8d558530d15f81
IAmIn <d.aminkafi@gmail.com>
2023-06-27 19:27:35 +0330

    logs added

--- src/Mano Hardware/ManoWebApplication.py
+++ src/Mano Hardware/ManoWebApplication.py
@@ -18,15 +18,18 @@ CORS(app)
 # API for testing
 @app.route("/")
 def hello():
-    app.logger.info('Received GET request')
+    app.logger.info('Received GET request on root')
     return "<p>Hello, Mano!</p>"
 
 # API for getting the programs from user and write into the files
 @app.route("/POST_programs", methods=['GET', 'POST'])
 def POST_programs():
-    app.logger.info('Received POST request')
-    app.logger.debug(f'Request data: {request.data}')
-    app.logger.debug(f'Request JSON: {request.json}')
+    if request.method == 'GET':
+        app.logger.info('Received GET request for POST_programs')
+    elif request.method == 'POST':
+        app.logger.info('Received POST request for POST_programs')
+        app.logger.debug(f'Request data: {request.data}')
+        app.logger.debug(f'Request JSON: {request.json}')
     main_program = request.json['main_program']
     microprogram = request.json['microprogram']
     # write the programs into the files
@@ -35,11 +38,12 @@ def POST_programs():
             json.dump(main_program, f)
         with open('microprogram_code.txt', 'w') as f:
             json.dump(microprogram, f)
+        app.logger.info('Programs written successfully')
+        return jsonify({'message': 'Programs written successfully'})
     except Exception as e:
         app.logger.error(f'Error writing programs: {e}')
         return jsonify({'error': str(e)})
-    app.logger.info('Programs written successfully')
-    return jsonify({'message': 'Programs written successfully'})
+    
 
 # extracting code
 assembly_program = open('main_code.txt', 'r').read().split('\n')
@@ -55,21 +59,26 @@ microassembler = MicroAssembler(assembly_microprogram)
 # API for assembling the microprogram code
 @app.route("/microassemble")
 def microassembler_assemble():
-    app.logger.info('Received GET request')
+    app.logger.info('Received GET request for microassemble')
     try:
         microassembler.assemble()
+        app.logger.info('Microprogram assembled successfully')
         return jsonify({'control message' : 'microprogram assembled succesfully', 'second pass table' : microassembler.get_second_pass_table(), 'first pass table' : microassembler.get_first_pass_table()})
     except Exception as e:
+        app.logger.error(f'Error assembling microprogram: {e}')
         return jsonify({'error' : str(e)})
 
 assembler = Assembler(assembly_program, microassembler.get_first_pass_table())
 # API for assembling the main program code
 @app.route("/assemble")
 def assembler_assemble():
+    app.logger.info('Received GET request for assemble')
     try:
         assembler.assemble()
+        app.logger.info('Program assembled successfully')
         return jsonify({'control message' : 'program assembled succesfully', 'second pass table' : assembler.get_second_pass_table(), 'first pass table' : assembler.get_first_pass_table()})
     except Exception as e:
+        app.logger.error(f'Error assembling program: {e}')
         return jsonify({'error' : str(e)})
 
 # initializing processor
@@ -77,6 +86,7 @@ processor = CPU(assembler.start_of_program, microassembler.start_of_microprogram
 # API for initializing the processor, just a quick show of processor status, not necessary
 @app.route("/initialize")
 def initialize():
+    app.logger.info('Received GET request for initialize')
     return jsonify({'registers' : processor.get_registers(),
                     'main memory' : processor.get_memory(),
                     'microprogram memory' : processor.get_microprogram_memory()})
@@ -86,18 +96,23 @@ programmer = programmer(processor, assembler.get_second_pass_table(), microassem
 # API for loading the programs to memories of processor
 @app.route("/load")
 def load():
+    app.logger.info('Received GET request for load')
     try:
         programmer.load_program()
         programmer.load_microprogram()
+        app.logger.info('Programs loaded successfully')
         return jsonify({'main memory' : processor.main_memory.get_memory(), 'microprogram memory' : processor.microprogram_memory.get_memory()})
     except Exception as e:
+        app.logger.error(f'Error loading programs: {e}')
         return jsonify({'error' : str(e)})
 
 # API for getting the status of processor after a single clock pulse
 @app.route("/microexecute")
 def microexecute():
+    app.logger.info('Received GET request for microexecute')
     try:
         processor.microexecute()
+        app.logger.info('Microexecuted successfully')
         return jsonify({'control message' : processor.get_microexecute_control_message(),
                         'registers' : processor.get_registers(),
                         'main memory' : processor.get_memory(),
@@ -105,13 +120,16 @@ def microexecute():
                         'microprogram memory' : processor.get_microprogram_memory(),
                         'microprogram' : assembly_microprogram})
     except Exception as e:
+        app.logger.error(f'Error microexecuting: {e}')
         return jsonify({'error' : str(e)})
     
 # API for getting the status of processor after execution of a single instruction
 @app.route("/execute")
 def execute():
+    app.logger.info('Received GET request for execute')
     try:
         processor.execute()
+        app.logger.info('Executed successfully')
         return jsonify({'control message' : processor.get_execute_control_message(),
                         'registers' : processor.get_registers(),
                         'main memory' : processor.get_memory(),
@@ -119,13 +137,16 @@ def execute():
                         'microprogram memory' : processor.get_microprogram_memory(),
                         'microprogram' : assembly_microprogram})
     except Exception as e:
+        app.logger.error(f'Error executing: {e}')
         return jsonify({'error' : str(e)})
 
 # API for running the whole program
 @app.route("/run")
 def run():
+    app.logger.info('Received GET request for run')
     try:
         processor.run()
+        app.logger.info('Program run successfully')
         return jsonify({'control message' : processor.get_run_control_message(),
                         'registers' : processor.get_registers(),
                         'main memory' : processor.get_memory(),
@@ -133,6 +154,7 @@ def run():
                         'microprogram memory' : processor.get_microprogram_memory(),
                         'microprogram' : assembly_microprogram})
     except Exception as e:
+        app.logger.error(f'Error running program: {e}')
         return jsonify({'error' : str(e)})
 
 
